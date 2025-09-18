950dff082c221ffd9d89f52fb923a4ca9680d1c9
rayaanr <rayaannsn@gmail.com>
2025-09-18 12:43:31 +0530

    refactor: remove unnecessary "use client" directive from campaign page

--- src/app/campaign/[id]/page.tsx
+++ src/app/campaign/[id]/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Navbar } from "@/components/layout/navbar";
 import { CampaignHeader } from "@/components/campaign-header";
 import { QuestionList } from "@/components/question-list";
